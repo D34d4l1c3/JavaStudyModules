d2d35aad3182f1dbb68147bc33d7accbc389f431
D34d4l1c3 <D34d4l1c3@gmail.com>
2024-06-06 15:47:02 +0300

    Итерация 6 - запросы к бд

--- src/main/java/org/example/iteration6/QueryExample.sql
+++ src/main/java/org/example/iteration6/QueryExample.sql
@@ -66,12 +66,13 @@ limit 10;
 
 
 -- выбрать по 10 наиболее популярных книг по всем категориям за последний квартал с сортировкой по популярности
-select count(b.id) as pop_bc, b.id, max(b.name),b.type as bk_name
-        from library.rent_log r inner join library.product_unit pu on pu.id = r.product_id
-        left join library.books b on b.id = pu.book_id
-        where r.start_date >= current_date - interval '3 months'
-       group by b.type,b.id
-       order by b.type,pop_bc desc;
+select count(b.id) as pop_bc, b.id, max(b.name), b.type as bk_name
+from library.rent_log r
+         inner join library.product_unit pu on pu.id = r.product_id
+         left join library.books b on b.id = pu.book_id
+where r.start_date >= current_date - interval '3 months'
+group by b.type, b.id
+order by b.type, pop_bc desc;
 --тут не уверена позже посмотреть
 
 
@@ -93,8 +94,15 @@ limit 10;
 -- комментарии к нижнему
 --- (популярность - наиболее часто экземпляры его книг(включая соавторство) берут в аренду)
 --- (3 САМЫЕ поплуярные книги а не поплуярные за квартал - поэтому отдельным селектом популярные книги)
-with popular_rent as (select r.id  as rent_id,pu.id  as unit_id,pu.book_id, a.name,
-                             b.name as bk_name,ba.author_id,r.start_date,r.till_date,r.status
+with popular_rent as (select r.id   as rent_id,
+                             pu.id  as unit_id,
+                             pu.book_id,
+                             a.name,
+                             b.name as bk_name,
+                             ba.author_id,
+                             r.start_date,
+                             r.till_date,
+                             r.status
                       from library.rent_log r
                                left join library.product_unit pu on pu.id = r.product_id
                                left join library.book_authors ba on ba.book_id = pu.book_id
@@ -126,34 +134,70 @@ order by popul desc;
 
 -- выбрать 100 наиболее активных читателей (критерии придумать самостоятельно)
 -- чаще всего берут в аренду книги и возвращают не нарушив срок
-select count(client_id) cnt_cl_per_rent,max(c.first_name) as fst_name,max(c.last_name) as lst_name
+select count(client_id) cnt_cl_per_rent, max(c.first_name) as fst_name, max(c.last_name) as lst_name
 from library.rent_log r
          inner join library.client_card cc on cc.id = r.rent_by_id
          left join library.clients c on c.id = cc.client_id
-where r.status ='Возвращено' or (r.status='Отдано' and r.till_date >= current_date)
-group by client_id order by cnt_cl_per_rent desc limit 100;
+where r.status = 'Возвращено'
+   or (r.status = 'Отдано' and r.till_date >= current_date)
+group by client_id
+order by cnt_cl_per_rent desc
+limit 100;
 
 -- выбрать 100 наиболее безответственных читателей, у которых больше всех просроченных возвратов
 -- выбрать 100 наиболее безответственных читателей, у которых больше всех утерь книг (оба условия один запрос условие поменять)
-select count(client_id) cnt_cl_per_rent,max(c.first_name) as fst_name,max(c.last_name) as lst_name
+select count(client_id) cnt_cl_per_rent, max(c.first_name) as fst_name, max(c.last_name) as lst_name
 from library.rent_log r
          inner join library.client_card cc on cc.id = r.rent_by_id
          left join library.clients c on c.id = cc.client_id
-where r.status ='Потеряно' or (r.status='Отдано' and r.till_date < current_date)
-group by client_id order by cnt_cl_per_rent desc limit 100;
+where r.status = 'Потеряно'
+   or (r.status = 'Отдано' and r.till_date < current_date)
+group by client_id
+order by cnt_cl_per_rent desc
+limit 100;
 
 -- выбрать количество выдач (возвратов) книг по дням за последний месяц
 with rent_month as (select *
-from library.rent_log r
-    where r.start_date >= current_date - interval '1 months')
-select start_date as event_day,count(id) ,'Отдано' as stat from rent_month t where t.status = 'Отдано' group by start_date
+                    from library.rent_log r
+                    where r.start_date >= current_date - interval '1 months')
+select start_date as event_day, count(id), 'Отдано' as stat
+from rent_month t
+where t.status = 'Отдано'
+group by start_date
 union all
-select return_date as event_day,count(id),'Возвращено' as stat from rent_month t where t.status = 'Возвращено' group by return_date
+select return_date as event_day, count(id), 'Возвращено' as stat
+from rent_month t
+where t.status = 'Возвращено'
+group by return_date
 
 --- выбрать самые пустующие полки / стеллажи
---- посчитать количество свободных мест для книг
---- посчитать количество всех книг / экземпляров книг / авторов / книг по каждому автору / экзмпляров книг по каждому автору /
 
+-- пустующие полки
+select count(current_unit) as cnt_unit, bookshelf_id,type
+from library.shelf_position
+         inner join library.bookcase_shelfs sh on bookshelf_id = sh.id
+group by bookshelf_id,type
+order by cnt_unit;
+
+-- пустующие стеллажи - на которых меньше всего юнитов(экземпляры книг\продукции) лежит
+select count(current_unit) as cnt_unit, bookcase_num
+from library.shelf_position
+         inner join library.bookcase_shelfs sh on bookshelf_id = sh.id
+group by sh.bookcase_num
+order by cnt_unit;
+
+--- посчитать количество свободных мест для книг
+select count(1)
+from library.shelf_position
+where current_unit is null
+
+--- посчитать количество всех книг / экземпляров книг / авторов / книг по каждому автору / экзмпляров книг по каждому автору /
+select count(1) as cnt_book,'Количествр книг' from library.books
+union all
+select count(1) as cnt_book,'Количество экземпляров книг' from library.product_unit;
+
+-- select count(1) as cnt_book,'Количествр книг по каждому автору' from library.books
+-- select count(1) as cnt_book,'Количествр книг' from library.books
 
 --and (r.return_date is not null or r.till_date >= current_date)
 -- select * from library.book_authors inner join library.books b2 on b2.id = book_authors.book_id
